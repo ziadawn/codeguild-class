aaf9228168029aab1cecf3ebe7d15135d65ee6fb
ziadawn <zmareck@gmail.com>
2017-10-03 14:09:36 -0700

    version 1

---
+++ Python Assignments/lab2-madlibs.py
@@ -0,0 +1,24 @@
+
+
+
+s = ''
+
+adjective = input('give me an adjective related to size: ')
+noun_1 = input('give me an thing or object: ')
+thinking = input('give me a synonym for thinking: ')
+position = input('give me a relative position, such as under or beside: ')
+noun_2 = input('give me a noun: ')
+noun_3 = input('give me another noun: ')
+noun_4 = input('give me one more noun: ')
+
+reply = 'twinkle twinkle ' + adjective + ' ' + noun_1 + '\nhow I ' + thinking + ' what you are. \n' \
+        + position + ' the ' + noun_2 + ' so high, \nlike a ' + noun_3 + ' in the ' + noun_4 + '.'
+
+print(reply)
+
+'''
+twinkle twinkle [little] [star]
+how i [wonder] what you are
+[up above] the [world] so high
+like a [diamond] in the [sky]
+'''

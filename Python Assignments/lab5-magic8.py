b2504756269fc01c86af7405b73275f779328471
ziadawn <zmareck@gmail.com>
2017-10-03 15:51:12 -0700

    version 1

--- Python Assignments/lab5-magic8.py
+++ Python Assignments/lab5-magic8.py
@@ -3,7 +3,7 @@
 import random
 
 answers = ['Looking good for you, cuite!', 'Sorry, try again tomorrow.', 'What is it worth to you?', 'If you make it worth my while.',
- 'Yes, but only if it is raining.', 'Absolutely!', 'Tragically no. I\'m sad about it, too.']
+ 'Yes, but only if it is raining.', 'Absolutely!', 'Tragically no. I\'m sad about it, too.', 'Not this time. Make a sacrifice to your gods and ask again.']
 
 question = input('Welcome, supplicant. What is your question? \n')
 answer = random.choice(answers)

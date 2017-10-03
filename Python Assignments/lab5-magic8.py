47b189b72976779817666f4937cb2021f65d9b9d
ziadawn <zmareck@gmail.com>
2017-10-03 15:49:48 -0700

    version 1

---
+++ Python Assignments/lab5-magic8.py
@@ -0,0 +1,20 @@
+
+
+import random
+
+answers = ['Looking good for you, cuite!', 'Sorry, try again tomorrow.', 'What is it worth to you?', 'If you make it worth my while.',
+ 'Yes, but only if it is raining.', 'Absolutely!', 'Tragically no. I\'m sad about it, too.']
+
+question = input('Welcome, supplicant. What is your question? \n')
+answer = random.choice(answers)
+print(answer)
+
+response = ''
+while response != 'done':
+    response = input('What is your next question? \n')
+    answer = random.choice(answers)
+
+    if response != 'done':
+        print(answer)
+
+print('Go forth, brave one, armed with your new knowledge!')

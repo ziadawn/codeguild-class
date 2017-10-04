376e783b68735f96b8002f45e410eb2a38033d8b
ziadawn <zmareck@gmail.com>
2017-10-04 11:06:55 -0700

    updated comments

--- Python Assignments/lab7-random-password.py
+++ Python Assignments/lab7-random-password.py
@@ -1,16 +1,24 @@
+'''
+lab 7, a code that generates a random password, of N length input by user, from a bucket of characters and special characters
+
+other variation to try:
+asking how many of each kind of things I need (capitals, numbers, special characters
+'''
+
 import random
 
+#these are split so I could ask for each part separately, but is overkill as currently written
 alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYabcdefghijklmnopqrstuvwxyz0123456789'
 special_characters = '!@#$%&'
 
-bucket = alphabet + special_characters
+bucket = alphabet + special_characters #combines above strings
 
 pw_need = input("How many characters do you need? ")
-pw_need = int(pw_need)
+pw_need = int(pw_need) #convert string to int
 
 i = 0
 n = pw_need
-password = ''
+password = '' #empty variable
 while i < n:
    password += random.choice(bucket)
    i = i + 1

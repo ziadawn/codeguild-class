f584b1604355e7bf4131af9bd5798a71e4a8df9f
ziadawn <zmareck@gmail.com>
2017-10-04 11:00:34 -0700

    updated comments

--- Python Assignments/lab5-magic8.py
+++ Python Assignments/lab5-magic8.py
@@ -1,18 +1,20 @@
+'''
+lab 5, a magic 8 ball, for all your probing questions!
+'''
 
-
-import random
+import random #Zia! remember to import random!!
 
 answers = ['Looking good for you, cuite!', 'Sorry, try again tomorrow.', 'What is it worth to you?', 'If you make it worth my while.',
  'Yes, but only if it is raining.', 'Absolutely!', 'Tragically no. I\'m sad about it, too.', 'Not this time. Make a sacrifice to your gods and ask again.']
 
 question = input('Welcome, supplicant. What is your question? \n')
-answer = random.choice(answers)
+answer = random.choice(answers) #if you forget to import random, then this part doesn't work.
 print(answer)
 
-response = ''
+response = '' #setting an empty variable, so it can be whatever I need?
 while response != 'done':
     response = input('What is your next question? \n')
-    answer = random.choice(answers)
+    answer = random.choice(answers) #this also doesn't work if you don't import random
 
     if response != 'done':
         print(answer)

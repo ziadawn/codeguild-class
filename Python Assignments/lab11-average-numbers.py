95361020a50b8d7c2164c860fabfac75447f8504
ziadawn <zmareck@gmail.com>
2017-10-04 16:25:11 -0700

    version 2 success!

--- Python Assignments/lab11-average-numbers.py
+++ Python Assignments/lab11-average-numbers.py
@@ -1,36 +1,53 @@
 '''
 Lab 11: Average Numbers
 
-We're going to average a list of numbers. Start with the following list, iterate through it, keeping a 'running sum',
-then divide that sum by the number of elements in that list. Remember len will give you the length of a list.
+Version 1: Calculate the average of a given list.
 
-The code below hows how to loop through an array, and prints the elements one at a time.
+Version 2
 
-nums = [5, 0, 8, 3, 4, 1, 6]
+Ask the user to enter the numbers one at a time, putting them into a list.
+If the user enters 'done', then calculate and display the average.
+The following code demonstrates how to add an element to the end of a list.
 
-# loop over the elements
-for num in nums:
-    print(num)
+nums = []
+nums.append(5)
+print(nums)
+Below is an example input/output:
 
-# loop over the indices
-for i in range(len(nums)):
-    print(nums[i])
-
-# we can use len and while to iterate over a list of arbitary size
-fruits = ['apple', 'banana', 'peach', 'pear']
-i = 0
-while i < len(fruits):
-    print(fruits[i])
-    i += 1
+> enter a number, or 'done': 5
+> enter a number, or 'done': 3
+> enter a number, or 'done': 4
+> enter a number, or 'done': done
+average: 4
 
 
 '''
 
+'''
+# Version 1:
 nums = [5, 0, 8, 3, 4, 1, 6]
+total = sum(nums)
+average = total / len(nums)
+print(average)
+'''
+
+#Version 2
+
+print('Hello! Please enter your numbers one at a time when prompted. Enter "done" when you have finished.')
+
+numbers = ' '
+nums = []
+
+numbers = input('Enter a number, or done: ')
+
+while numbers != 'done':
+    nums.append(int(numbers))
+    numbers = input('Enter a number, or done: ')
+
+
+#print(nums)
 
 total = sum(nums)
-
 average = total / len(nums)
-
 print(average)
 

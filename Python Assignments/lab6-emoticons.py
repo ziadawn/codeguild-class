2240dd5e51a0e0657764b4c1935c317e265abbec
ziadawn <zmareck@gmail.com>
2017-10-04 11:04:01 -0700

    updated comments

--- Python Assignments/lab6-emoticons.py
+++ Python Assignments/lab6-emoticons.py
@@ -1,8 +1,13 @@
 '''
-Version 3, next thing to try! Can I have optional hats or things that only sometimes show up?
+Lab 6, a random generator to make emoticons from a set list of eyes, noses, and mouths
+
+Other ideas to try?
+Can I have optional hats or things that only sometimes show up?
+Another variation: computer asks which ones I like, and stores them. Then I can choose from previously chosen ones or new, random ones?
+Can I have the computer not use rejected ones again?
 '''
 
-import random
+import random #remember to do this!
 
 eyes = [':', ';', '8', 'B', '=']
 noses = [' ', 'o', '-', '+']
@@ -12,6 +17,7 @@ mouths = [')', '(', 'D', '*', 'P', '|']
 
 i = 0
 while i < 5:
+    #these are inside the while loop so I get five different emoticons, rather than five of the same, which happened before
     eye = random.choice(eyes)
     nose = random.choice(noses)
     mouth = random.choice(mouths)
@@ -19,6 +25,6 @@ while i < 5:
     emoticon = eye + nose + mouth
 
     print(emoticon)
-    i = i + 1
+    i = i + 1 #add this so that it does not give me an infinite list of emoticons
 
 

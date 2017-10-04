5b1fea6f0b9bbbdf478e7b9c34547af4684e2e4f
ziadawn <zmareck@gmail.com>
2017-10-04 10:57:46 -0700

    updated comments

--- Python Assignments/lab2-madlibs.py
+++ Python Assignments/lab2-madlibs.py
@@ -1,7 +1,8 @@
+'''
+lab 2, write a madlib
 
+'''
 
-
-s = ''
 
 adjective = input('give me an adjective related to size: ')
 noun_1 = input('give me an thing or object: ')
@@ -11,12 +12,17 @@ noun_2 = input('give me a noun: ')
 noun_3 = input('give me another noun: ')
 noun_4 = input('give me one more noun: ')
 
+#I've considered just asking for four nouns at once and then slotting them in, but moved on to another lab
+
 reply = 'twinkle twinkle ' + adjective + ' ' + noun_1 + '\nhow I ' + thinking + ' what you are. \n' \
         + position + ' the ' + noun_2 + ' so high, \nlike a ' + noun_3 + ' in the ' + noun_4 + '.'
 
 print(reply)
 
+
 '''
+I have the original here so I could decide which words to replace
+
 twinkle twinkle [little] [star]
 how i [wonder] what you are
 [up above] the [world] so high

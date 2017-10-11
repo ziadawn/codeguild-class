16ab35ee8db2f3cff5d4f3e4a1e00bd5e017c577
ziadawn <zmareck@gmail.com>
2017-10-11 12:20:54 -0700

    version 2, tested for 1-999. Breaks if put in 1000 or more: a change for another day

--- Python Assignments/Week 2 Python/lab17_number_to_phrase.py
+++ Python Assignments/Week 2 Python/lab17_number_to_phrase.py
@@ -9,15 +9,59 @@ Version 3 (optional)
 Convert a number to roman numerals.
 '''
 
-digit = input('What is the number? ')
+num = int(input('What is the number? '))
+
+ones_list = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+teens_list = ["ten", 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+tens_list = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+
+output = ''
+
+def ones_place(num):        # should rename this variable to something that makes more sense
+    if num < 10:
+        return ones_list[num]
+    elif num < 20:
+        return teens_list[num - 10]     # because teen - 10 = the place in the list
+    elif num < 100:
+        tens_digit = num // 10
+        ones_digit = num % 10
+        if ones_digit == 0:
+            return tens_list[tens_digit - 2]
+        else:
+            return tens_list[tens_digit - 2] + '-' + ones_list[ones_digit]
+
+
+def hundreds_place(num):
+    tens_remain = num % 100             # this is a NEW VARIABLE, which we set to pull from ones_list, to find the hundreds place
+    hundreds_digit = num // 100
+
+    hundreds_list = ones_list[hundreds_digit] + ' hundred'
+
+    if tens_remain > 0:                 # this line means that whole hundreds won't print "four hundred zero" or what have you
+        return hundreds_list + ' ' + ones_place(tens_remain)    # reference the new variable here
+    else:
+        return hundreds_list
+
+
+if num > 100:
+    print(hundreds_place(num))
+else:
+    print(ones_place(num))
+
+
+
+'''
+Original version:
+
+digit = int(input('What is the number? '))
 
 ones_list = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 teens_list = ["ten", 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
 tens_list = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
 
-
-tens_digit = int(digit)//10
-ones_digit = int(digit) % 10
+hundreds_digit = digit // 100
+tens_digit = digit // 10
+ones_digit = digit % 10
 
 nums = ''
 
@@ -30,3 +74,14 @@ if tens_digit != 1 and ones_digit != 0:
     nums += (ones_list[ones_digit-1])
 
 print(nums)
+
+other draft:
+# if num < 1000 and num > 100:
+#     output += ones_list[hundreds_digit] + ' hundred' + (tens_list[tens_digit - 2]) + ' ' + ones_list[ones_digit]
+# elif num < 100 and num > 20:
+#     output += (tens_list[tens_digit - 2]) + ' ' + ones_list[ones_digit]
+# elif num < 20 and num > 10:
+#     output += teens_list[ones_digit]    # this line also works
+# elif num < 10:
+#     output += ones_list[ones_digit]     # this line works
+'''

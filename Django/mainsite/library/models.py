29ead691d2054761fd7fe612ed5dc274aa185907
ziadawn <zmareck@gmail.com>
2017-12-15 14:45:12 -0800

    fixed a bug

--- Django/mainsite/library/models.py
+++ Django/mainsite/library/models.py
@@ -12,7 +12,7 @@ class Author(models.Model):
 
 
 class Book(models.Model):
-    author = models.ForeignKey(Author)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     publish_date = models.CharField(max_length=200)
 
